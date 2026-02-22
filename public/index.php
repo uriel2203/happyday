b5905d4a298ea2cb62322575f049fbed0790c7c9
uriel2203 <urieljohnchavez@gmail.com>
2026-02-22 16:58:42 +0800

    fix: commit build assets and skip vercel build steps3

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
+<?php
+
+use Illuminate\Http\Request;
+
 // --- Vercel Compatibility Shim ---
 if (getenv('WEB_KEY')) {
     putenv('APP_KEY=' . getenv('WEB_KEY'));
@@ -24,7 +28,5 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
