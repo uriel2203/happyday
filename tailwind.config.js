ac18ce59922aa2fbe768eb3fbbcd38d297939dc2
uriel2203 <urieljohnchavez@gmail.com>
2026-02-22 18:05:51 +0800

    TOTAL OVERHAUL: Converted to Static Frontend project for Vercel compatibility

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+    content: [
+        "./index.html",
+        "./events/*.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                'outfit': ['Outfit', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
